00efd3e9d306e53eb260e26009c49a917fa6278c
gblanchard4 <gblanchard4@gmail.com>
2015-08-02 21:58:28 -0500

    need time delay for api overload

--- geopy_dwi.py
+++ geopy_dwi.py
@@ -4,8 +4,12 @@ import time
 
 geolocator = GoogleV3()
 
-with open("dwi.address",'r') as addresses, open("dwi.address.geocode", 'w') as output:
+with open("DWI_2011_2014.csv",'r') as addresses, open("dwi.address.geocode.csv", 'w') as output:
 	for line in addresses:
-		location = geolocator.geocode(line.rstrip('\n'))
-		time.sleep(.5)
-		output.write("{},{},{}\n".format(line.rstrip('\n'), location.latitude, location.longitude))
+		location = geolocator.geocode(line.rstrip('\n').split(',')[1])
+		try: 
+			output.write("{},{},{}\n".format(line.rstrip('\n'), location.latitude, location.longitude))
+			time.sleep(.5)
+		except AttributeError:
+			print line
+
