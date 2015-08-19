6de2a87c44aa40dc2a67f5f6bdb1b5f74979843c
Gene Blanchard <gblanchard4@gmail.com>
2015-08-18 22:22:52 -0500

    teh googz

---
+++ geocoding/geopy_dwi_google.py
@@ -0,0 +1,24 @@
+# Geopy DWI
+from geopy.geocoders import GoogleV3
+from geopy.exc import GeocoderTimedOut
+
+import time
+
+geolocator = GoogleV3()
+
+
+#with open("2011_DWI.csv",'r') as addresses, open("2011_DWI.geo.csv", 'w') as output, open("2011_DWI.errors.csv", 'w') as err:
+#with open("2012_DWI.csv",'r') as addresses, open("2012_DWI.geo.csv", 'w') as output, open("2012_DWI.errors.csv", 'w') as err:
+with open("2013_DWI.csv",'r') as addresses, open("2013_DWI.geo.csv", 'w') as output, open("2013_DWI.errors.csv", 'w') as err:
+#with open("2014_DWI.csv",'r') as addresses, open("2014_DWI.geo.csv", 'w') as output, open("2014_DWI.errors.csv", 'w') as err:
+	for line in addresses:
+		location = geolocator.geocode(line.rstrip('\n').split(',')[1]+"New Orelans", timeout=10)
+		try:
+			output.write("{},{},{}\n".format(line.rstrip('\n'), location.latitude, location.longitude))
+			time.sleep(1)
+		except AttributeError:
+			err.write(line)
+			time.sleep(1)
+		except GeocoderTimedOut:
+			print "timeout"
+			err.write(line)
